db783ac908c8ffe4e11b06a40e92382baaa366bb
Stansfield, Treyden <trstans@iu.edu>
2024-12-20 12:27:56 -0500

    Final test of R version
    It seems key functions are missing

--- MiniMonsterPlex.r
+++ MiniMonsterPlex.r
@@ -5,10 +5,15 @@ library("ggtext")
 library("Rsamtools")
 library("Rbowtie2")
 
-accumulate_fn <- get(args$f)
-
 bowtie2_samtools(bt2Index = "index/70-15_index",output = 'test',outputType = "bam",seq1 = "fastq/104.fq.gz",
                  bamFile=NULL,seq2 = NULL,'--no-unal -p 8 --local --very-sensitive-local',interleaved = TRUE)
+
 sortBam("test.bam","test_sorted")
 
-sys.call()
+indexBam(file = "test_sorted.bam")
+
+
+
+test.vcf = pileup("test_sorted.bam",index="test_sorted.bam.bai",
+       "--threads 8 -d 100000 -R MonsterPlexRegionsFileSuperCont.txt --annotate FORMAT/AD -f index/70-15.fasta.fasta ")
+
